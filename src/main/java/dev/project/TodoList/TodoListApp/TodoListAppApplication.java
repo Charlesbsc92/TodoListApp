f661219805658aed3ffbe899aa48e1035c7cffc7
Charlesbsc92 <charlesbsc92@gmail.com>
2025-11-05 07:40:57 +0530

    [Feature] Intial spring web setup completed.

---
+++ src/main/java/dev/project/TodoList/TodoListApp/TodoListAppApplication.java
@@ -0,0 +1,13 @@
+package dev.project.TodoList.TodoListApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListAppApplication.class, args);
+	}
+
+}
